64423a7eecb6116b7c9c0abdb5cdce6b1b911637
Matteo Seclì <secli.matteo@gmail.com>
2020-09-14 09:53:51 +0200

    Update qtermwidget.rb

--- qtermwidget.rb
+++ qtermwidget.rb
@@ -3,6 +3,7 @@ class Qtermwidget < Formula
   homepage "https://github.com/lxde/qtermwidget"
   url "https://f1ash.fedorapeople.org/qt-virt-manager/QTermWidget5/qt5-qtermwidget.zip"
   sha256 "0a371c3d7ff2cc4cd33526b34733d24380dc5f51dbafb6fa7ad0e7f49568d2b5"
+  version "0.2.0"
 
   depends_on "libtool" => :build
   depends_on "pkg-config" => :build
